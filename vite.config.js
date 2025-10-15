af02923886edaea3f4c34c9f23c9152db30f6224
Alexandra Moldovan <alexandra.moldovan03@e-uvt.ro>
2025-10-15 16:36:21 +0300

    modificari vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        auth: 'auth.html',
+        galerie: 'galerie.html',
+        shop: 'shop.html',
+        // adaugă aici orice alt .html pe care vrei să-l servești
+        admin: 'admin.html',
+        anulare: 'anulare.html',
+        confidentialitatecookie: 'confidentialitate-cookie.html',
+        contact: 'contact.html',
+        dashboard: 'dashboard.html',
+        galerie: 'galerie.html',
+        index: 'index.html',
+        reset: 'reset.html',
+        retur: 'retur.html',
+        shopCategory: 'shop-category.html',
+        success: 'succes.html',
+        termeni: 'termeni.html',
+        updatePassword: 'update-password.html'
+
+
+
+      }
+    }
+  }
+});
